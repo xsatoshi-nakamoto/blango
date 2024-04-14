68f88fa1a65bbac78d280b5af0c2ded5973bfd81
vinh nguyen <quangvinh.1305@gmail.com>
2024-04-14 06:35:34 +0000

    self training safe use format html

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,28 +2,27 @@ from django.contrib.auth import get_user_model
 from django import template
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
-
+from django.utils.html import format_html
 register = template.Library()
 
 user_model = get_user_model()
 
 @register.filter
 def author_details(author):
-  if not isinstance(author, user_model):
-    # return empty string as safe default
-    return ""
+    if not isinstance(author, user_model):
+        # return empty string as safe default
+        return ""
 
-  if author.first_name and author.last_name:
-    name = escape(f"{author.first_name} {author.last_name}")
-  else:
-    name = escape(f"{author.username}")
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
 
-  if author.email:
-    email = escape(author.email)
-    prefix = f'<a href="mailto:{email}">'
-    suffix = "</a>"
-  else:
-    prefix = ""
-    suffix = ""
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
 
-  return mark_safe(f"{prefix}{name}{suffix}")
+    return format_html('{}{}{}', prefix, name, suffix)
