db846f9ca99c91f5893831772de371f897d4d9bb
Satoshi Nakamoto <xsatoshi.nakamoto@gmail.com>
2024-04-15 08:53:57 +0000

    Finish crispy forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from crispy_forms.layout import Submit
+from crispy_forms.helper import FormHelper
 from blog.models import Comment
 
 
@@ -7,3 +9,8 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["content"]
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Submit'))
