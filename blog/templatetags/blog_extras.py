a4c2d41ce0b4e500d9e9d55b28e1a5a3ffec2f13
vinh nguyen <quangvinh.1305@gmail.com>
2024-04-15 03:22:11 +0000

    add blogi self trainingwq

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -3,6 +3,7 @@ from django import template
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
 from django.utils.html import format_html
+from blog.models import Post
 register = template.Library()
 
 user_model = get_user_model()
@@ -33,5 +34,43 @@ def row():
 
 
 @register.simple_tag
+def endcol():
+    return format_html("</div>")
+
+@register.simple_tag
+def col():
+    return format_html('<div class="col">')
+
+
+@register.simple_tag
 def endrow():
     return format_html("</div>")
+
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+    request = context["request"]
+    current_user = request.user
+    post = context["post"]
+    author = post.author
+
+    if author == current_user:
+        return format_html("<strong>me</strong>")
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+
+    return format_html("{}{}{}", prefix, name, suffix)
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
