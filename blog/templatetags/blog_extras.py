7173720b1495d64d880fad491d69c4da63bedf9e
vinh nguyen <quangvinh.1305@gmail.com>
2024-04-18 10:21:29 +0000

    Finish caching

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -4,6 +4,8 @@ from django.utils.html import escape
 from django.utils.safestring import mark_safe
 from django.utils.html import format_html
 from blog.models import Post
+import logging
+logger = logging.getLogger(__name__)
 register = template.Library()
 
 user_model = get_user_model()
@@ -72,5 +74,6 @@ def author_details_tag(context):
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
     posts = Post.objects.exclude(pk=post.pk)[:5]
+    logger.debug("Loaded %d recent posts for post %d", len(posts), post.pk)
     return {"title": "Recent Posts", "posts": posts}
 
--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,12 @@ from blog.models import Post
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from blog.forms import CommentForm
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 import logging
 logger = logging.getLogger(__name__)
-
+@cache_page(300)
+@vary_on_cookie
 def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
   logger.debug("Got %d posts", len(posts))
