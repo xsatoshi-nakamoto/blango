546fda86bbf28dd5bbed3d7836e227437b3741bf
vinh nguyen <quangvinh.1305@gmail.com>
2024-04-28 02:31:41 +0000

    add react

--- static/blog/blog.js
+++ static/blog/blog.js
@@ -1,1 +1,37 @@
-alert('Hello, world!')
+class ClickButton extends React.Component {
+  state = {
+    wasClicked: false
+  }
+
+  handleClick () {
+    this.setState(
+      {wasClicked: true}
+    )
+  }
+
+  render () {
+    let buttonText
+
+    if (this.state.wasClicked)
+      buttonText = 'Clicked!'
+    else
+      buttonText = 'Click Me'
+
+    return React.createElement(
+      'button',
+      {
+        className: 'btn btn-primary mt-2',
+        onClick: () => {
+          this.handleClick()
+        }
+      },
+      buttonText
+    )
+  }
+}
+
+const domContainer = document.getElementById('react_root')
+ReactDOM.render(
+  React.createElement(ClickButton),
+  domContainer
+)
